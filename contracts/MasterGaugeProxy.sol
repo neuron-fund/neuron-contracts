c2f0bb9f26f84787b1bf4662cec8461cbf19c9f3
Avele <kotoraxia@gmail.com>
2021-05-31 01:13:18 +0400

    gauge proxy update

--- contracts/MasterGaugeProxy.sol
+++ contracts/MasterGaugeProxy.sol
@@ -16,6 +16,7 @@ contract GaugeProxy is ProtocolGovernance {
     MasterChef public immutable MASTER;
     IERC20 public immutable NEURON;
     IERC20 public immutable AXON;
+    address public immutable TREASURY;
 
     uint256 public pid;
     uint256 public totalWeight;
@@ -30,11 +31,13 @@ contract GaugeProxy is ProtocolGovernance {
     constructor(
         address _masterChef,
         address _neuronToken,
-        address _axon
+        address _axon,
+        address _treasury
     ) public {
         MASTER = MasterChef(_masterChef);
         NEURON = NeuronToken(_neuronToken);
-        AXON = _axon;
+        AXON = IERC20(_axon);
+        TREASURY = _treasury;
         governance = msg.sender;
     }
 
@@ -72,6 +75,7 @@ contract GaugeProxy is ProtocolGovernance {
     }
 
     // Adjusts _owner's votes according to latest _owner's AXON balance
+    // TODO функция вызывается ботом как я понял
     function poke(address _owner) public {
         address[] memory _tokenVote = tokenVote[_owner];
         uint256 _tokenCnt = _tokenVote.length;
@@ -130,46 +134,28 @@ contract GaugeProxy is ProtocolGovernance {
         _vote(msg.sender, _tokenVote, _weights);
     }
 
-    // Add new token gauge
     function addGauge(address _token) external {
         require(msg.sender == governance, "!gov");
         require(gauges[_token] == address(0x0), "exists");
-        gauges[_token] = address(new Gauge(_token));
+        gauges[_token] = address(
+            new Gauge(_token, address(NEURON), address(AXON), address(TREASURY))
+        );
         _tokens.push(_token);
-    }
-
-    // Sets MasterChef PID
-    function setPID(uint256 _pid) external {
-        require(msg.sender == governance, "!gov");
-        require(pid == 0, "pid has already been set");
-        require(_pid > 0, "invalid pid");
-        pid = _pid;
-    }
-
-    // Deposits mDILL into MasterChef
-    function deposit() public {
-        require(pid > 0, "pid not initialized");
-        IERC20 _token = TOKEN;
-        uint256 _balance = _token.balanceOf(address(this));
-        _token.safeApprove(address(MASTER), 0);
-        _token.safeApprove(address(MASTER), _balance);
-        MASTER.deposit(pid, _balance);
     }
 
     // Fetches Pickle
     function collect() public {
-        (uint256 _locked, ) = MASTER.userInfo(pid, address(this));
-        MASTER.withdraw(pid, _locked);
-        deposit();
+        MASTER.mintTokensForGauge();
     }
 
     function length() external view returns (uint256) {
         return _tokens.length;
     }
 
+    // TODO данная функция вызывается раз в неделю ссудя по логам на etherscan
     function distribute() external {
         collect();
-        uint256 _balance = PICKLE.balanceOf(address(this));
+        uint256 _balance = NEURON.balanceOf(address(this));
         if (_balance > 0 && totalWeight > 0) {
             for (uint256 i = 0; i < _tokens.length; i++) {
                 address _token = _tokens[i];
@@ -177,8 +163,8 @@ contract GaugeProxy is ProtocolGovernance {
                 uint256 _reward =
                     _balance.mul(weights[_token]).div(totalWeight);
                 if (_reward > 0) {
-                    PICKLE.safeApprove(_gauge, 0);
-                    PICKLE.safeApprove(_gauge, _reward);
+                    NEURON.safeApprove(_gauge, 0);
+                    NEURON.safeApprove(_gauge, _reward);
                     Gauge(_gauge).notifyRewardAmount(_reward);
                 }
             }
