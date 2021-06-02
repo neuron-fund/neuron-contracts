86bae67ec0a049461529bc91d273bbe7776f5896
Avele <kotoraxia@gmail.com>
2021-06-02 17:20:49 +0400

    constants

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,43 +1,46 @@
 
-    export const NeuronTokenAddress = '0x6A54c7C8bfF6Ce0380C7FD19aB2A764e860AD3BD'
-    export const ControllerAddress = '0xFfDb48703CEBEB4e120f1D9BF93a5C4b3f2Cf8bb'
-    export const MasterChefAddress = '0x3fD1DE9Af029BB3057f606334aD611790649F648'
+    export const NeuronTokenAddress = '0x5A5e94e769fB3d64C12167d8353F95f1DbD141FF'
+    export const ControllerAddress = '0xb022163d55c0790e19AB15387D96B4741F0ABA14'
+    export const MasterChefAddress = '0xB3Aa3C6bf83E513df49c4b0BcB281ba2081E260D'
+    export const GaugeDistributorAddress = '0xa80720F72689ed8Bb1d84626d1511F76ad72B9d8'
 
     export const Pools = {
-
+      
         Curve3Crv: {
-          strategyAddress: '0x7009D8b928FA7a44cE39E453D3fe9217A61E29F2',
-          poolAddress: '0x621fBEe6F3feCb12110137C50DE54BaC639df06A',
-          poolIndex: 0,
+          strategyAddress: '0x196d551C9c3DbCE2F9Eee29FF30415Cdf890B0B0',
+          poolAddress: '0xC77Af597df6E7612Ae4501c0eeDc2507779aDC78',
+          gaugeAddress: '0xf50e5E836645A818471c623c760a7B437D76eC08',
           inputTokenAddress: '0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490',
-          inputTokenSymbol: '3Crv'
+          inputTokenSymbol: '3Crv',
         },
 
-
+      
         CurveRenCrv: {
-          strategyAddress: '0x073FAcf9DDDC77ae7581FCACe76ce211D6A29994',
-          poolAddress: '0xca8d2968Aa4E305bc9aFB12Bf0805F74b63354FF',
-          poolIndex: 1,
+          strategyAddress: '0x6453151cFD73a8F67c27d5da7785467dee7053EF',
+          poolAddress: '0x6f0633b3f9c600aa59aD39758578d65C08b4d69B',
+          gaugeAddress: '0x58a4A6b05126Eb32d5737875d9D2bE638D9d8bb3',
           inputTokenAddress: '0x49849C98ae39Fff122806C06791Fa73784FB3675',
-          inputTokenSymbol: 'crvRenWBTC'
+          inputTokenSymbol: 'crvRenWBTC',
         },
 
-
+      
         CurveSteCrv: {
-          strategyAddress: '0x105cDF66620ef655209226761b377029DfF93E58',
-          poolAddress: '0x2370D997183D0F5D4C7AC5b52bec36a34F118A68',
-          poolIndex: 2,
+          strategyAddress: '0x7a671bF6E50fEDF54eB890F006b1eb59b9235C30',
+          poolAddress: '0xDB67Fd25b30EA00dB47BEC42ad5E8904C5E51d2D',
+          gaugeAddress: '0x762FCDdf47343e90639A96353d39e49995695e4b',
           inputTokenAddress: '0x06325440D014e39736583c165C2963BA99fAf14E',
-          inputTokenSymbol: 'steCRV'
+          inputTokenSymbol: 'steCRV',
         },
 
-
+      
         FeiTribeLp: {
-          strategyAddress: '0xf4C265CA0f8E7DDcDD317484b69dE07525Cfd72A',
-          poolAddress: '0x30870a2e00B2f540358ac1a3F323Df4ae54598B3',
-          poolIndex: 3,
+          strategyAddress: '0x26094Bc49d742698480994faA026301dEcBe268e',
+          poolAddress: '0xdD401a7d5B6De58395EC98e5ac2bDc0833fAA8B6',
+          gaugeAddress: '0xf4B8B51041312E5825608272Ff46B257B7e50Bb5',
           inputTokenAddress: '0x9928e4046d7c6513326cCeA028cD3e7a91c7590A',
-          inputTokenSymbol: 'UNI-V2'
+          inputTokenSymbol: 'UNI-V2',
         },
+
+      
     }
   
