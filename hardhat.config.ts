3ef1d154e7b3860462b7ce83e77611961ec98b1f
Avele <kotoraxia@gmail.com>
2021-08-29 06:39:07 +0300

    ido contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,13 +17,26 @@ import "@typechain/hardhat"
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.2',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
+    compilers: [
+      {
+        version: '0.6.12',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: '0.8.2',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+    ],
   },
   abiExporter: {
     path: './abi'
