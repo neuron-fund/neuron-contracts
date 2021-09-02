0ead7b94a40eacc9b9cdd21e2575e7746b62b2b2
Avele <kotoraxia@gmail.com>
2021-09-02 18:22:14 +0300

    deploy prod scripts

---
+++ scripts/deployProdAndWriteConstants.ts
@@ -0,0 +1,14 @@
+import { deploy } from './deployProd'
+import { writeConstantsFile } from './writeConstantsFile'
+
+async function main () {
+  const deployedAddresses = await deploy()
+  writeConstantsFile(deployedAddresses, 'mainnetAddresses')
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
