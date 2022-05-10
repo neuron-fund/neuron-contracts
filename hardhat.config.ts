a65ee3e6e4734b12b15315b81d9bb9af75c010ae
Avele <kotoraxia@gmail.com>
2022-05-10 16:20:30 +0400

    format

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,21 @@
-import { config as dontenvConfig } from 'dotenv'
+import {config as dontenvConfig} from 'dotenv'
 import dotenvExpand from 'dotenv-expand'
 const env = dontenvConfig()
 dotenvExpand(env)
-import { testPrivateKeys } from './utils/testPrivateKeys'
+import {testPrivateKeys} from './utils/testPrivateKeys'
 
-import { HardhatUserConfig } from "hardhat/config"
-import "@nomiclabs/hardhat-ethers"
-import "@nomiclabs/hardhat-waffle"
-import "@nomiclabs/hardhat-vyper"
-import "hardhat-deploy-ethers"
-import "hardhat-deploy"
-import "@nomiclabs/hardhat-web3"
+import {HardhatUserConfig} from 'hardhat/config'
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-vyper'
+import 'hardhat-deploy-ethers'
+import 'hardhat-deploy'
+import '@nomiclabs/hardhat-web3'
 import 'hardhat-abi-exporter'
-import "@typechain/hardhat"
-import "hardhat-gas-reporter"
-import "@nomiclabs/hardhat-etherscan"
-import "@openzeppelin/hardhat-upgrades"
+import '@typechain/hardhat'
+import 'hardhat-gas-reporter'
+import '@nomiclabs/hardhat-etherscan'
+import '@openzeppelin/hardhat-upgrades'
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -25,30 +25,31 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200
-          }
-        }
+            runs: 200,
+          },
+        },
       },
       {
         version: '0.8.2',
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200
-          }
-        }
+            runs: 200,
+          },
+        },
       },
     ],
   },
   abiExporter: {
-    path: './abi'
+    path: './abi',
   },
   networks: {
     hardhat: {
       chainId: process.env.POLYGON ? 1339 : 1337,
       forking: {
-        url: process.env.POLYGON ? process.env.ALCHEMY_POLYGON : process.env.ALCHEMY,
-        blockNumber: Number(process.env.FORK_BLOCK_NUMBER) || undefined,
+        url: process.env.POLYGON
+          ? process.env.ALCHEMY_POLYGON
+          : process.env.ALCHEMY,
       },
       loggingEnabled: true,
       blockGasLimit: 0x1fffffffffffff,
@@ -58,25 +59,28 @@ const config: HardhatUserConfig = {
     // prodMainnet: {
     //   url: process.env.PROD_MAINNET_RPC,
     //   gasPrice: 40e9,
-    //   blockGasLimit: 5e6
+    //   blockGasLimit: 5e6,
     // },
     // prodPolygon: {
     //   url: process.env.PROD_POLYGON_RPC,
     // },
-    // localPolygon: {
-    //   url: 'http://127.0.0.1:8546/',
-    //   accounts: getHardhatAccounts(20).map(x => x.privateKey)
-    // },
-    // testnet: {
-    //   url: 'https://neurontestnet.xyz/',
-    //   accounts: getHardhatAccounts(20).map(x => x.privateKey)
-    // }
+    localPolygon: {
+      url: 'http://127.0.0.1:8546/',
+      accounts: getHardhatAccounts(20).map(x => x.privateKey),
+    },
+    testnet: {
+      url: 'https://neurontestnet.xyz/',
+      accounts: getHardhatAccounts(20).map(x => x.privateKey),
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API,
   },
   // etherscan: {
   //   apiKey: process.env.ETHERSCAN_API
   // },
   vyper: {
-    version: '0.2.12'
+    version: '0.2.12',
   },
   mocha: {
     reporter: 'eth-gas-reporter',
@@ -90,16 +94,14 @@ const config: HardhatUserConfig = {
   // }
 }
 
-function getHardhatAccounts (accountsNumber: number) {
-  return testPrivateKeys
-    .map(x => {
-      const A_LOT_OF_ETH = "100000000000000000000000000000000000000000"
-      return {
-        privateKey: x,
-        balance: A_LOT_OF_ETH,
-      }
-    })
+function getHardhatAccounts(accountsNumber: number) {
+  return testPrivateKeys.map(x => {
+    const A_LOT_OF_ETH = '100000000000000000000000000000000000000000'
+    return {
+      privateKey: x,
+      balance: A_LOT_OF_ETH,
+    }
+  })
 }
-
 
 export default config
