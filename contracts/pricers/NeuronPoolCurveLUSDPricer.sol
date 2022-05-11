be7217d37d01cd32eb3d5600d2065e8ec02e1897
inveker <alex.korolev.1995@gmail.com>
2022-05-11 14:57:40 +0300

    lusd pricer 1-fees

--- contracts/pricers/NeuronPoolCurveLUSDPricer.sol
+++ contracts/pricers/NeuronPoolCurveLUSDPricer.sol
@@ -9,13 +9,9 @@ import {INeuronPool} from "../interfaces/INeuronPool.sol";
 import {ITroveManager} from "../interfaces/ITroveManager.sol";
 
 contract NeuronPoolCurveLUSDPricer is IPricer {
-
     INeuronPool public immutable NEURON_POOL;
 
     IPricer public immutable CRV3_PRICER;
-
-    AggregatorV3Interface public constant DAI_PRICER =
-        AggregatorV3Interface(0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9);
 
     ITroveManager public constant TROVE_MANAGER =
         ITroveManager(0xA39739EF8b0231DbFA0DcdA07d7e29faAbCf4bb2);
@@ -30,9 +26,7 @@ contract NeuronPoolCurveLUSDPricer is IPricer {
 
     function getPrice() external view override returns (uint256) {
         uint256 crv3Price = CRV3_PRICER.getPrice();
-        (, int256 daiPrice, , , ) = DAI_PRICER.latestRoundData();
-        uint256 lusdPrice = (uint256(daiPrice) *
-            (1e18 - TROVE_MANAGER.getRedemptionRate())) / 1e8;
+        uint256 lusdPrice = 1e18 - TROVE_MANAGER.getRedemptionRate();
 
         return
             (NEURON_POOL.pricePerShare() *
